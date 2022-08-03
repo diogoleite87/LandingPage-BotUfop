48054510258e7f80ab1113c4a9681cf252864488
Diogo Leite <diogoleite87@gmail.com>
2022-08-03 00:32:06 -0300

    update: add information on the pages

--- src/pages/BotUfopRU.tsx
+++ src/pages/BotUfopRU.tsx
@@ -1,3 +1,6 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPython } from '@fortawesome/free-brands-svg-icons'
+
 import Container from "../components/Container"
 
 export default function BotUfopRU () {
@@ -5,24 +8,20 @@ export default function BotUfopRU () {
         <Container title="bot ufop ru">
                 <>
                     <>
-                        <h2>Pergunta?</h2>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis recusandae beatae numquam delectus veritatis corporis magni ipsam non, quod iste facere distinctio dolorem nam quos iure vel soluta ipsum pariatur.</p>
+                        <h2>Em qual linguagem de programação foi feito?</h2>
+                        <p>Feito inteiro em Python <FontAwesomeIcon icon={faPython} />, utiliza API do Twitter e faz Web Scraping com BeautifulSoup.</p>
                     </>
                     <>
-                        <h2>Pergunta?</h2>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis recusandae beatae numquam delectus veritatis corporis magni ipsam non, quod iste facere distinctio dolorem nam quos iure vel soluta ipsum pariatur.</p>
+                        <h2>Como funciona?</h2>
+                        <p>Explicando de forma que todos possam entender, ele faz um Web Scraping, basicamente ele abre o navegador e extrai todo o HTML (textos) do site do cardápio na UFOP, logo após obter esses dados, ele filtra e formata bonitinho para ser postado, respeitando o limite de 280 caracteres do twitter.</p>
                     </>
                     <>
-                        <h2>Pergunta?</h2>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis recusandae beatae numquam delectus veritatis corporis magni ipsam non, quod iste facere distinctio dolorem nam quos iure vel soluta ipsum pariatur.</p>
+                        <h2>Hoje ele não postou, o que pode ter acontecido?</h2>
+                        <p>A UFOP nem sempre atualiza o site do cardápio e mesmo quando atualiza, as vezes não atualizam "certo" e/ou não atualiza os 2 cardápios no dia.</p>
                     </>
                     <>
-                        <h2>Pergunta?</h2>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis recusandae beatae numquam delectus veritatis corporis magni ipsam non, quod iste facere distinctio dolorem nam quos iure vel soluta ipsum pariatur.</p>
-                    </>
-                    <>
-                        <h2>Pergunta?</h2>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis recusandae beatae numquam delectus veritatis corporis magni ipsam non, quod iste facere distinctio dolorem nam quos iure vel soluta ipsum pariatur.</p>
+                        <h2>Legal, mas onde eu vejo a estrutura dele?</h2>
+                        <p>O código inteiro esta disponível no meu GitHub, basta copiar ou clicar no link: <a href="https://github.com/diogoleite87/BOT-TWITTER-UFOP-RU" target="_blank">github.com/diogoleite87/BOT-TWITTER-UFOP-RU</a></p>
                     </>
                 </>
         </Container>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,24 +6,32 @@ export default function Home() {
             <Container title="Início">
                 <>
                     <>
-                        <h2>Pergunta?</h2>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis recusandae beatae numquam delectus veritatis corporis magni ipsam non, quod iste facere distinctio dolorem nam quos iure vel soluta ipsum pariatur.</p>
+                        <h2>Os BOTs são da UFOP?</h2>
+                        <p>Não! A UFOP não tem relação/vínculos com os BOTs em questão, foram feitos e são mantidos por um discente (Aluno).</p>
                     </>
                     <>
-                        <h2>Pergunta?</h2>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis recusandae beatae numquam delectus veritatis corporis magni ipsam non, quod iste facere distinctio dolorem nam quos iure vel soluta ipsum pariatur.</p>
+                        <h2>O criador ganha algo com eles?</h2>
+                        <p>Não! Foram construídos com intuito de que alguma forma ajude a comunidade acadêmica e também de mim (<a href="https://github.com/diogoleite87" target="_blank">criador</a>) colocar em prática os meus conhecimentos e estudos.</p>
                     </>
                     <>
-                        <h2>Pergunta?</h2>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis recusandae beatae numquam delectus veritatis corporis magni ipsam non, quod iste facere distinctio dolorem nam quos iure vel soluta ipsum pariatur.</p>
+                        <h2>Se não ganha nada, como eles são mantidos?</h2>
+                        <p>Bem, existem algumas ferramentas online na qual a hospedagem de códigos simples é feita de forma gratuita. Neste caso, ambos são hospedados pela Heroku.</p>
                     </>
                     <>
-                        <h2>Pergunta?</h2>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis recusandae beatae numquam delectus veritatis corporis magni ipsam non, quod iste facere distinctio dolorem nam quos iure vel soluta ipsum pariatur.</p>
+                        <h2>O BOT aparentemente não funciona mais, e agora?</h2>
+                        <p>Como eu sou um usuário assíduo do twitter e também aluno da UFOP, provavelmente eu já devo ter reparado. Entretanto, geralmente eu estou atolado de afazeres, então assim que tiver um tempinho eu vou arrumar. Mas, sinta-se a vontade para mandar mensagem em uma das redes sociais disponíveis no rodapé desse site (serio, pode mandar).</p>
                     </>
                     <>
-                        <h2>Pergunta?</h2>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis recusandae beatae numquam delectus veritatis corporis magni ipsam non, quod iste facere distinctio dolorem nam quos iure vel soluta ipsum pariatur.</p>
+                        <h2>Vai manter eles funcionando para sempre?</h2>
+                        <p>Nem viver para sempre eu vou e também vou formar um dia e enfim... Se isso acontecer e alguém quiser assumir eles a partir daí, eu passo eles para esse alguém.</p>
+                    </>
+                    <>
+                        <h2>Código bastante amador, não acha?</h2>
+                        <p>Acho não, na verdade, eu tenho certeza. Quando eu criei, principalmente o BOT de datas, estava no começo do curso e mal sabia o que era uma variável, então me dê um desconto :(</p>
+                    </>
+                    <>
+                        <h2>Possui alguma dúvida, dica ou simplesmente quer falar algo?</h2>
+                        <p>Pode me mandar uma mensagem em alguma das redes sociais disponíveis no rodapé desse site ou se preferir pode simplesmente mandar um email para diogoleite87@gmail.com</p>
                     </>
                 </>
             </Container>
